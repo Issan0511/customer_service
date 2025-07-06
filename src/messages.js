195b6606cee8d20cf5ae976733fdf3c08085ee48
宇野慎一郎 <cgej0918@mail3.doshisha.ac.jp>
2025-07-06 23:47:45 +0900

    Update follow message with clear call to action

--- src/messages.js
+++ src/messages.js
@@ -1,12 +1,17 @@
-function createFormLinkMessage(link, prefix = '') {
+function createFormLinkMessage(
+  link,
+  prefix = '',
+  text = '以下のフォームから情報を入力してください。',
+  buttonLabel = 'フォームを開く'
+) {
   return {
     type: 'template',
     altText: 'お客様サポートフォーム',
     template: {
       type: 'buttons',
-      text: `${prefix}\n以下のフォームから情報を入力してください。`,
+      text: [prefix, text].filter(Boolean).join('\n'),
       actions: [
-        { type: 'uri', label: 'フォームを開く', uri: link }
+        { type: 'uri', label: buttonLabel, uri: link }
       ]
     }
   };
